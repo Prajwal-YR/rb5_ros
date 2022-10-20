784e31e1e0707c7547247f2f3d6a57324a8fdaba
Prajwal-YR <yr.prajwal@gmail.com>
2022-10-20 23:16:05 +0000

    Camera calibrations for april detection

--- april_detection/src/april_detection_node.cpp
+++ april_detection/src/april_detection_node.cpp
@@ -18,16 +18,11 @@ ros::Publisher apriltag_pub;
 ros::Subscriber image_sub;
 AprilDetection det;
 
-
 double distortion_coeff[] = {
-  -0.004690, 
-  -0.010306, 
-  -0.000044, 
-  0.000303, 
-  0.000000};
-double intrinsics[] = {671.42099,    0.     ,  969.0114,
-                       0.     ,  670.67143,  555.81758,
-                       0.     ,    0.     ,    1.};
+    -0.302938, 0.062472, 0.028313, -0.006054, 0.000000};
+double intrinsics[] = {705.984775, 0.000000, 651.112684,
+                       0., 673.767438, 219.253853,
+                       0., 0., 1.};
 
 const cv::Mat d(cv::Size(1, 5), CV_64FC1, distortion_coeff);
 const cv::Mat K(cv::Size(3, 3), CV_64FC1, intrinsics);
@@ -117,7 +112,7 @@ int main(int argc, char *argv[]){
 
   pose_pub = n.advertise<geometry_msgs::PoseArray>("/april_poses", 10); 
   apriltag_pub = n.advertise<april_detection::AprilTagDetectionArray>("/apriltag_detection_array", 10);
-  image_sub = n.subscribe("/camera_0", 1, imageCallback);
+  image_sub = n.subscribe("/camera_1", 1, imageCallback);
   
   // Set info for pose estimation
   // details from: https://github.com/AprilRobotics/apriltag/wiki/AprilTag-User-Guide#pose-estimation
